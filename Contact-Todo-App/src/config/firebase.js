b6758a0e028dd8b680b823affb9314ee2640c082
shahsaaqib <shahsaaqib01@gmail.com>
2024-09-13 20:45:57 +0530

    env added

--- Contact-Todo-App/src/config/firebase.js
+++ Contact-Todo-App/src/config/firebase.js
@@ -1,18 +1,22 @@
-import { firebaseCred } from "./firebaseCred";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
+const apiKey = import.meta.env.VITE_FIREBASE_API_KEY;
+const authDomain = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
+const projectId = import.meta.env.VITE_FIREBASE_PROJECT_ID;
+const storageBucket = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
+const messagingSenderId = import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
+const appId = import.meta.env.VITE_FIREBASE_APP_ID;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: firebaseCred.FIREBASE_API_KEY,
-  authDomain: firebaseCred.FIREBASE_AUTH_DOMAIN,
-  projectId: firebaseCred.FIREBASE_PROJECT_ID,
-  storageBucket: firebaseCred.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: firebaseCred.FIREBASE_MESSAGING_SENDER_ID,
-  appId: firebaseCred.FIREBASE_APP_ID,
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
 };
 
 // Initialize Firebase
